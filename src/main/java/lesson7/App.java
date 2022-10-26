bb474005d359929be13e0b8fb30eba9674b63c4c
Baurzhan Dosym <baurzhan.zh@gmail.com>
2022-10-25 21:30:05 -0400

    Lesson 7 - Enums

---
+++ src/main/java/lesson7/App.java
@@ -0,0 +1,17 @@
+package lesson7;
+
+public class App {
+    public static void main(String[] args) {
+        Colors car = Colors.RED;
+        System.out.println(car);
+        car = Colors.BLUE;
+        System.out.println(car);
+
+        car = Colors.WHITE;
+
+        // "RED" "red" "Red"
+
+        SchoolType newDorpHighSchool = SchoolType.PUBLIC;
+
+    }
+}
