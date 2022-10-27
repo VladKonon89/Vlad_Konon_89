90e93af9c4bef16411f209e22c26565c7d718b82
dandubitsky <dan.dubitsky@gmail.com>
2022-10-27 13:41:54 -0400

    Git->Commit

---
+++ src/main/java/Lesson8dd/Color.java
@@ -0,0 +1,9 @@
+package Lesson8dd;
+
+public enum Color {
+    BLACK,
+    SILVER,
+    WHITE,
+    PINK
+
+}
---
+++ src/main/java/Lesson8dd/Person.java
@@ -0,0 +1,9 @@
+package Lesson8dd;
+
+public class Person {
+    public String name;
+    public String LastName;
+    public int year;
+
+
+}
