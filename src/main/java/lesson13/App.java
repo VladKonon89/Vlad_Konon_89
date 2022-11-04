3114e6958bd8883356e503b556b855843ef53e1e
Baurzhan Dosym <baurzhan.zh@gmail.com>
2022-11-03 21:25:45 -0400

    Lesson 13 - Collections

---
+++ src/main/java/lesson13/App.java
@@ -0,0 +1,26 @@
+package lesson13;
+
+public class App {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = a;
+        a = 17;
+//        System.out.println("b="+b);
+
+        Person anna = new Person("Anna");
+        Person student = anna;
+        anna.setName("Anita");
+//        System.out.println(student.getName());
+
+        Person anita = new Person("Anita");
+        System.out.println(student==anna);
+//        System.out.println(anita==anna);
+
+
+        String str1 = "Hello1";
+        String str2 = "Hello";
+//        System.out.println(str2.equals(str1));
+        System.out.println(anna.equals(anita));
+
+    }
+}
