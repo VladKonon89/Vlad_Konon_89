7868baa85397cd0b6f4fb3ba255d3659564ae768
VladKonon89 <volodymyrkononenko89@gmail.com>
2022-11-14 22:46:58 +0100

    Selenium_VK89

---
+++ src/test/java/simpleTest_vk/SimpleTest.java
@@ -0,0 +1,10 @@
+package simpleTest_vk;
+
+import org.testng.annotations.Test;
+
+public class SimpleTest {
+    @Test
+    public void run(){System.setProperty("web.chrome.driver","chromedriver");
+
+    }
+}
