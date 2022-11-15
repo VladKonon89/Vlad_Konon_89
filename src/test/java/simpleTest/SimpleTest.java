69c4d39cb582901350a3725303ae839f583e703a
Baurzhan Dosym <baurzhan.zh@gmail.com>
2022-11-14 21:34:21 -0500

    Lesson 4 - Advanced XPath

--- src/test/java/simpleTest/SimpleTest.java
+++ src/test/java/simpleTest/SimpleTest.java
@@ -1,11 +1,21 @@
 package simpleTest;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class SimpleTest {
     @Test
     public void run(){
         System.setProperty("web.chrome.driver","chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://bbb.testpro.io");
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException xx){}
+
+        driver.quit();
 
     }
 }
