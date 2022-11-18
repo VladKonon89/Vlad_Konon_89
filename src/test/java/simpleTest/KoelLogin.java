06793121ccca4ac5028984d10e2dc005822dcfa7
Baurzhan Dosym <baurzhan.zh@gmail.com>
2022-11-17 21:20:02 -0500

    Lesson 7 - Synchronization

--- src/test/java/simpleTest/KoelLogin.java
+++ src/test/java/simpleTest/KoelLogin.java
@@ -1,22 +1,36 @@
 package simpleTest;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
 import java.util.List;
 
 public class KoelLogin {
     private WebDriver driver;
+    WebDriverWait wait;
+    FluentWait<WebDriver> fluentWait;
+
     @BeforeMethod
-    public void startUp(){
-        System.setProperty("web.chrome.driver","chromedriver.exe");
+    public void startUp() {
+        System.setProperty("web.chrome.driver", "chromedriver.exe");
         driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10, 200);
+        fluentWait = new FluentWait<>(driver)
+                .withTimeout(Duration.ofSeconds(10))
+                .pollingEvery(Duration.ofMillis(100))
+                .ignoring(NoSuchElementException.class)
+                .ignoring(ElementClickInterceptedException.class)
+                .ignoring(StaleElementReferenceException.class);
+//        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
         driver.get("https://bbb.testpro.io");
     }
 
@@ -25,6 +39,7 @@ public class KoelLogin {
         Thread.sleep(100);
         driver.quit();
     }
+
     @Test
     public void loginToKoel_correctCredentials() throws InterruptedException {
         By emailFieldLocator = By.xpath("//*[@type='email']");
@@ -40,13 +55,14 @@ public class KoelLogin {
         passwordField.sendKeys("te$t$tudent");
         loginButton.click();
 
-        Thread.sleep(2000);
-
         By homeLocator = By.className("home");
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(homeLocator));
         WebElement homeIcon = driver.findElement(homeLocator);
 
         Assert.assertTrue(homeIcon.isDisplayed());
     }
+
     @Test
     public void loginToKoel_incorrectCredentials() throws InterruptedException {
 
@@ -63,15 +79,50 @@ public class KoelLogin {
         passwordField.sendKeys("WrongPassword");
         loginButton.click();
 
-        Thread.sleep(300);
+        By errorLocator = By.className("error");
+
+//        fluentWait.until(x->x.findElement(errorLocator));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(errorLocator));
+
+        WebElement errorFrame = driver.findElement(errorLocator);
+
+        Assert.assertTrue(errorFrame.isDisplayed());
+    }
+
+    @Test
+    public void customWait() throws InterruptedException {
+
+        By emailFieldLocator = By.xpath("//*[@type='email']");
+        WebElement emailField = driver.findElement(emailFieldLocator);
+
+        By passwordFieldLocator = By.cssSelector("[type='password']");
+        WebElement passwordField = driver.findElement(passwordFieldLocator);
+
+        By loginButtonLocator = By.tagName("button");
+        WebElement loginButton = driver.findElement(loginButtonLocator);
+
+        emailField.sendKeys("sim@email.com");
+        passwordField.sendKeys("WrongPassword");
+        loginButton.click();
 
         By errorLocator = By.className("error");
+
+        for (int i=0;i<50;i++){
+            System.out.println(i);
+            try {
+                driver.findElement(errorLocator);
+                break;
+            }catch (NoSuchElementException err){
+
+            }
+        }
+
         WebElement errorFrame = driver.findElement(errorLocator);
 
         Assert.assertTrue(errorFrame.isDisplayed());
     }
     @Test
-    public void sum(){
+    public void sum() {
         List<WebElement> elements = driver.findElements(By.tagName("button"));
         System.out.println(elements.size());
         driver.navigate().refresh();
@@ -85,7 +136,7 @@ public class KoelLogin {
 //        blueButton.isSelected();
         WebElement blueButton = driver.findElement(By.tagName("button"));
         String innerText = blueButton.getText();
-        System.out.println("it = "+innerText);
+        System.out.println("it = " + innerText);
         String value = blueButton.getAttribute("type");
         System.out.println(value);
         String bg = blueButton.getCssValue("background");
