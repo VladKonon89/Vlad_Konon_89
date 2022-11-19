403addeaa835d3c529a5be8ad14725fa68262d4c
Baurzhan Dosym <baurzhan.zh@gmail.com>
2022-11-18 21:20:05 -0500

    Lesson 7 - PageObject 1

---
+++ src/test/java/pageObjectTests/LoginToApp.java
@@ -0,0 +1,43 @@
+package pageObjectTests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pageObjects.LoginPage;
+import pageObjects.MainPage;
+
+public class LoginToApp {
+    private WebDriver driver;
+    private String url;
+
+    @BeforeMethod
+    public void startUp() {
+        System.setProperty("web.chrome.driver", "chromedriver.exe");
+        driver = new ChromeDriver();
+        url="https://bbb.testpro.io";
+    }
+
+    @AfterMethod
+    public void tearDown() throws InterruptedException {
+        Thread.sleep(100);
+        driver.quit();
+    }
+    @Test
+    public void loginToApp_correctCredentials(){
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.open(url);
+        MainPage mainPage = loginPage.loginToApp("sim@email.com","te$t$tudent");
+        Assert.assertTrue(mainPage.isOpen());
+    }
+    @Test
+    public void loginToApp_incorrectCredentials(){
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.open(url);
+        loginPage.loginToApp("sim@email.com","WrongPassword");
+        Assert.assertTrue(loginPage.isError());
+    }
+
+}
