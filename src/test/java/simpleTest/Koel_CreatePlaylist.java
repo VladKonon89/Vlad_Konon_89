7ec4f0826e0b859bac94d260b066febeef53fea4
VladKonon89 <volodymyrkononenko89@gmail.com>
2022-11-28 06:26:35 +0100

    JavaSelenium - Lesson 7 - Synchronization - HomeWork _VK89

--- src/test/java/simpleTest/Koel_CreatePlaylist.java
+++ src/test/java/simpleTest/Koel_CreatePlaylist.java
@@ -1,19 +1,40 @@
 package simpleTest;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
+
 public class Koel_CreatePlaylist {
+    private WebDriver driver;
+    WebDriverWait wait;
+
+    @BeforeMethod
+    public void startUp() {
+        System.setProperty("web.chrome.driver", "chromedriver.exe");
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 15, 300);
+
+        driver.get("https://bbb.testpro.io");
+    }
+
+    @AfterMethod
+    public void tearDown() throws InterruptedException {
+        Thread.sleep(100);
+        driver.quit();
+    }
     @Test
     public void loginToKoel_correctCredentials() throws InterruptedException {
-        System.setProperty("web.chrome.driver", "chromedriver");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://bbb.testpro.io");
+//        System.setProperty("web.chrome.driver", "chromedriver");
+//        WebDriver driver = new ChromeDriver();
+//        wait = new WebDriverWait(driver,15,400);
+//        driver.get("https://bbb.testpro.io");
 
         By emailFieldLocator = By.xpath("//*[@type='email']");
         WebElement emailField = driver.findElement(emailFieldLocator);
@@ -28,41 +49,45 @@ public class Koel_CreatePlaylist {
         passwordField.sendKeys("te$t$tudent");
         loginButton.click();
 
-        Thread.sleep(2000);
+//        Thread.sleep(2000);
 
         By createPlaylistLocator = By.xpath("//*[@title=\'Create a new playlist\']");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(createPlaylistLocator));
         WebElement createPlaylistButton = driver.findElement(createPlaylistLocator);
         createPlaylistButton.click();
 
-        Thread.sleep(2000);
+//        Thread.sleep(2000);
 
         By newPlaylistLocator = By.xpath("//*[@data-testid=\"playlist-context-menu-create-simple\"]");
         WebElement newPlaylistButton = driver.findElement(newPlaylistLocator);
         newPlaylistButton.click();
 
-        Thread.sleep(1000);
+//        Thread.sleep(1000);
 
         By playlistNameLocator = By.xpath("//*[@placeholder=\"↵ to save\"]");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(playlistNameLocator));
         WebElement playlistNameField = driver.findElement(playlistNameLocator);
 
         playlistNameField.sendKeys("Champion");
 
-        Thread.sleep(2000);
+//        Thread.sleep(2000);
 
         driver.findElement(By.xpath("//input[@placeholder=\"↵ to save\"]")).sendKeys(Keys.RETURN);
 
-        Thread.sleep(2000);
+//        Thread.sleep(2000);
 
         By championPlaylistLocator = By.xpath("//*[@href=\'#!/playlist/30586\']");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(championPlaylistLocator));
         WebElement championPlaylist = driver.findElement(championPlaylistLocator);
 
         Assert.assertTrue(championPlaylist.isDisplayed());
+
 
 //        By createdIconLocator = By.xpath("class=\"success show\"");
 //        WebElement createdIcon = driver.findElement(createdIconLocator);
 //        Assert.assertTrue(createdIcon.isDisplayed());
 
-        driver.quit();
+//        driver.quit();
 
     }
 
