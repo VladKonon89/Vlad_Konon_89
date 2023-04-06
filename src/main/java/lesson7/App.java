c54a26d9a33a833e38955f4f8ad1806c91711582
VladKonon89 <volodymyrkononenko89@gmail.com>
2023-04-06 03:45:31 +0200

    JavaSelenium - Lesson 9 - PageObjects 3_VK89

--- src/main/java/lesson7/App.java
+++ src/main/java/lesson7/App.java
@@ -2,17 +2,35 @@ package lesson7;
 
 public class App {
     public static void main(String[] args) {
-        Colors car = Colors.RED;
+        Colors car = Colors.RED;   // car - переменная
         System.out.println(car);
         car = Colors.BLUE;
         System.out.println(car);
 
         car = Colors.WHITE;
 
+        // энумы нужны для ограничения в ошибке, не можем использовать то, что не задано энумом
 
-        // "RED" "red" "Red"
-
-        SchoolType newDorpHighSchool = SchoolType.PUBLIC;
-
+        SchoolType newDorpHighSchool = SchoolType.PRIVATE;
+        System.out.println(newDorpHighSchool);
     }
 }
+
+
+
+//public class App {
+//    public static void main(String[] args) {
+//        Colors car = Colors.RED;
+//        System.out.println(car);
+//        car = Colors.BLUE;
+//        System.out.println(car);
+//
+//        car = Colors.WHITE;
+//
+//
+//        // "RED" "red" "Red" - разные красные для компьютера
+//
+//        SchoolType newDorpHighSchool = SchoolType.PUBLIC;
+//
+//    }
+//}
